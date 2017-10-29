163a7cf6f393d5c004ca0bb6992c9b6b52afa772
Minhaz Ahamed <Minhaz Ahamed@DESKTOP-VUMGVO2>
2017-10-29 18:09:45 -0500

    added new lines

--- Minhaz01/src/Minhaz01/Minhaz01.java
+++ Minhaz01/src/Minhaz01/Minhaz01.java
@@ -3,7 +3,8 @@ package Minhaz01;
 public class Minhaz01 {
 
 	public static void main(String[] args) {
-            System.out.println("My first Git project1");
+            System.out.println("My first Git project12355");
+            System.out.println("test 22222");
 	}
 
 }
