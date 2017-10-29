18883cc34b5a5c225bee7b8ed7097a85044eb2e5
Minhaz Ahamed <Minhaz Ahamed@DESKTOP-VUMGVO2>
2017-10-29 17:55:46 -0500

    test2

--- Minhaz01/src/Minhaz01/Minhaz01.java
+++ Minhaz01/src/Minhaz01/Minhaz01.java
@@ -3,7 +3,7 @@ package Minhaz01;
 public class Minhaz01 {
 
 	public static void main(String[] args) {
-            System.out.println("My first Git project");
+            System.out.println("My first Git project1");
 	}
 
 }
