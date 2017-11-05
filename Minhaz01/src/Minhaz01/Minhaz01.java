f62d23372686be813ca8801f6b1ec21ad1c91f0d
chitra01 <33205792+chitra01@users.noreply.github.com>
2017-11-05 14:03:08 -0500

    Update Minhaz01.java

--- Minhaz01/src/Minhaz01/Minhaz01.java
+++ Minhaz01/src/Minhaz01/Minhaz01.java
@@ -4,7 +4,7 @@ public class Minhaz01 {
 
 	public static void main(String[] args) {
             System.out.println("My first Git project12355");
-            System.out.println("test 22222");
+            System.out.println("test 33333");
 	}
 
 }
